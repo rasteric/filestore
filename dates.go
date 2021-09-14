cee1fb140f371ac67dc469cd060c62d8a790d8cc
E. H. Rast <erich@snafu.de>
2021-09-14 13:35:17 +0100

    Fixed wrong date spec in parseDBDate.

--- dates.go
+++ dates.go
@@ -3,7 +3,7 @@ package filestore
 import "time"
 
 func parseDBDate(date string) (time.Time, error) {
-	layout := "2006-01-02T15:04:05-0700"
+	layout := "2006-01-02 15:04:05"
 	return time.Parse(layout, date)
 }
 
